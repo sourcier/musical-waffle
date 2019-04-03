e10bd8413cf9eddf3f3c51702cd5642bdf6e3e51
Roger Rajaratnam <roger@sourcier.uk>
2019-04-03 11:54:52 +0100

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="ui top fixed menu">
+          <div className="item">
+            <img src={logo} />
+          </div>
+          <a className="item">Blog</a>
+        </div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
