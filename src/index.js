0e10de77452fc65ff0c9de0f23c689a7ed41ee7b
Roger Rajaratnam <roger@sourcier.uk>
2019-07-19 10:46:07 +0100

    Configure react snap

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import { render, hydrate } from "react-dom"
 import { Provider } from "react-redux"
 import App from "./App"
 import * as serviceWorker from "./serviceWorker"
 import "bootstrap/dist/css/bootstrap.min.css"
 import configureStore from "./store/configureStore"
 
-ReactDOM.render(
-    <Provider store={configureStore()}>
-        <App />
-    </Provider>,
-    document.getElementById("root")
-)
+const rootElement = document.getElementById("root")
+
+rootElement.hasChildNodes()
+    ? hydrate(
+        <Provider store={configureStore()}>
+            <App />
+        </Provider>,
+        document.getElementById("root")
+    )
+    : render(
+        <Provider store={configureStore()}>
+            <App />
+        </Provider>,
+        document.getElementById("root")
+    )
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
