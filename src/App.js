880f4cc603ddd27d8f222bdf5bf1a974181ce609
Roger Rajaratnam <roger@sourcier.uk>
2019-10-15 21:42:39 +0100

    Integrate with react app

--- src/App.js
+++ src/App.js
@@ -9,22 +9,14 @@ import Login from './pages/Login'
 import { Meta } from './components/Meta'
 import SignUp from './pages/SignUp'
 import Logout from './pages/Logout'
+import { Root } from './pages/Root'
 
 export default class App extends Component {
     render() {
         return (
             <BrowserRouter>
-                <Meta />
-                <Masthead />
                 <Switch>
-                    <Route exact path={'/'} component={BlogList} />
-                    <Route exact path={'/contact'} component={Contact} />
-                    <Route exact path={'/blog'} component={BlogList} />
-                    <Route exact path={'/blog/:slug'} component={BlogPost} />
-                    <Route exact path={'/login'} component={Login} />
-                    <Route exact path={'/logout'} component={Logout} />
-                    <Route exact path={'/signup'} component={SignUp} />
-                    <Route component={NotFound} />
+                    <Route path="/" component={Root} />
                 </Switch>
             </BrowserRouter>
         )
