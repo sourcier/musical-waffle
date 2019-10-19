c95ea6aa3ce128b024f7c69729b7681dcb5ad0f6
Roger Rajaratnam <roger@sourcier.uk>
2019-10-19 16:58:44 +0100

    Create Footer component

--- src/pages/Root/index.js
+++ src/pages/Root/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Header } from './Header'
 import { Hero } from './Hero'
+import { Footer } from './Footer'
 
 export const Root = ({ children }) => (
     <React.Fragment>
         <Header />
         <Hero />
         {children}
+        <Footer />
     </React.Fragment>
 )
