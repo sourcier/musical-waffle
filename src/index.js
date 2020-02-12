fcdf28ed145cfc619a51b21872113338266c8a5e
Roger Rajaratnam <roger@sourcier.uk>
2020-02-12 18:54:29 +0000

    Remove Parse.js

--- src/index.js
+++ src/index.js
@@ -4,22 +4,19 @@ import { Provider } from 'react-redux'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import configureStore from './store/configureStore'
-import { restoreUserSession } from './store/reducers/session'
 
 const rootElement = document.getElementById('root')
 
-restoreUserSession().then((session) => {
-    rootElement.hasChildNodes()
-        ? hydrate(
-              <Provider store={configureStore({ session })}>
-                  <App />
-              </Provider>,
-              document.getElementById('root')
-          )
-        : render(
-              <Provider store={configureStore({ session })}>
-                  <App />
-              </Provider>,
-              document.getElementById('root')
-          )
-})
+rootElement.hasChildNodes()
+    ? hydrate(
+          <Provider store={configureStore()}>
+              <App />
+          </Provider>,
+          document.getElementById('root')
+      )
+    : render(
+          <Provider store={configureStore()}>
+              <App />
+          </Provider>,
+          document.getElementById('root')
+      )
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux'
 import blog from './blog'
-import session from './session'
 
 export default combineReducers({
     blog,
-    session,
 })
