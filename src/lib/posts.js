988111092af226560a65c3699a35e10559638776
Roger Rajaratnam <roger@sourcier.uk>
2020-02-12 18:35:54 +0000

    Set auth mode for api

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -1,4 +1,4 @@
-import Amplify, { API, graphqlOperation } from 'aws-amplify'
+import Amplify, { API } from 'aws-amplify'
 import config from '../aws-exports'
 import * as queries from '../graphql/queries'
 
@@ -6,16 +6,21 @@ Amplify.configure(config)
 
 export const getAllPosts = () =>
     Promise.resolve()
-        .then(() => API.graphql(graphqlOperation(queries.listPosts)))
+        .then(() =>
+            API.graphql({
+                query: queries.listPosts,
+                authMode: 'API_KEY',
+            })
+        )
         .then((allPosts) => allPosts.data.listPosts.items)
 
 export const getPostBySlug = (slug) =>
     Promise.resolve()
         .then(() =>
-            API.graphql(
-                graphqlOperation(queries.listPosts, {
-                    $filter: { slug: { eg: slug } },
-                })
-            )
+            API.graphql({
+                query: queries.listPosts,
+                variables: { $filter: { slug: { eg: slug } } },
+                authMode: 'API_KEY',
+            })
         )
         .then((allPosts) => allPosts.data.listPosts.items[0])
