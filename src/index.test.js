a14f14161820db18b8878cfd54c8de560d4d89e2
Roger Rajaratnam <roger@sourcier.uk>
2020-02-13 14:43:19 +0000

    Fix build and tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import App from './index'
+import App from './pages/index'
 
 it('renders without crashing', () => {
     const component = shallow(<App restoreSession={jest.fn()} />)
