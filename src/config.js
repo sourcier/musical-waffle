935e9330b327e17f1251368bd2cf16b663eccccc
Roger Rajaratnam <roger@sourcier.uk>
2020-04-14 21:21:00 +0100

    Move serverless variables to parameters

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export default {
+  apiGateway: {
+    REGION: 'eu-west-2',
+    URL: 'https://zlgc1i8l0f.execute-api.eu-west-2.amazonaws.com/dev',
+  },
+  cognito: {
+    REGION: 'eu-west-2',
+    USER_POOL_ID: 'eu-west-2_EmAVZKFow',
+    APP_CLIENT_ID: '4gup9dvge5ngcpjdm1n1au2gdh',
+    IDENTITY_POOL_ID: 'eu-west-2:035f655f-27ca-454e-9684-f174f8ed3122',
+  },
+}
