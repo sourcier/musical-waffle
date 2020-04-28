2ecaf20ea6cf9a2eedb97313327f8a10ca3bfc6d
Roger Rajaratnam <roger@sourcier.uk>
2020-04-28 01:35:36 +0100

    Create menu toggle button

--- packages/web/src/components/Layout/header/index.js
+++ packages/web/src/components/Layout/header/index.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 import styles from './styles'
 import { withStyles } from '../../withStyles'
 
-const Header = ({ styles }) => <div css={styles.header} id="header" />
+const Header = ({ styles }) => {
+  return (
+    <div css={styles.header} id="header">
+      <div css={styles.toggle}>
+        <button>
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+      </div>
+    </div>
+  )
+}
 
 export default withStyles(styles)(Header)
--- packages/web/src/components/Layout/header/styles.js
+++ packages/web/src/components/Layout/header/styles.js
@@ -2,4 +2,11 @@ export default (props, { rhythm, colors }) => ({
   header: {
     borderTop: `solid ${rhythm(0.25)} ${colors.primary}`,
   },
+  toggle: {
+    textAlign: 'right',
+    padding: rhythm(0.5),
+    button: {
+      border: 0,
+    },
+  },
 })
