7c5b1761893d0959ce4006de93b7370d0ddeb464
Roger Rajaratnam <roger@sourcier.uk>
2020-04-30 19:56:30 +0100

    Setup clean webpack plugin

--- packages/web/webpack.prod.js
+++ packages/web/webpack.prod.js
@@ -1,8 +1,10 @@
 const path = require("path")
-const common = require("./webpack.common.js")
 const merge = require("webpack-merge")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const CopyWebpackPlugin = require("copy-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const common = require("./webpack.common.js")
 
 module.exports = merge(common, {
   mode: "production",
@@ -25,6 +27,7 @@ module.exports = merge(common, {
         to: path.join(__dirname, "build", "assets"),
       },
     ]),
+    new CleanWebpackPlugin(),
   ],
   optimization: {
     moduleIds: "hashed",
