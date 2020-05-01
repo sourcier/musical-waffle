c788f1d7c28034b533545f360828c78a602aacd6
Roger Rajaratnam <roger@sourcier.uk>
2020-05-01 17:03:31 +0100

    Make <Meta/> a functional component

--- packages/web/src/components/Meta.js
+++ packages/web/src/components/Meta.js
@@ -1,18 +1,8 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet-async'
 
-export class Meta extends React.Component {
-  static propTypes = {
-    title: PropTypes.string,
-  }
-
-  render() {
-    const { title } = this.props
-    return (
-      <Helmet>
-        <title>{title ? `${title} - ` : ``}Sourcier</title>
-      </Helmet>
-    )
-  }
-}
+export const Meta = ({ title }) => (
+  <Helmet>
+    <title>{title ? `${title} - ` : ``}Sourcier</title>
+  </Helmet>
+)
