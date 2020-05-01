f3334a8cdde2bb4175c1fd2d912318e5b36f8daa
Roger Rajaratnam <roger@sourcier.uk>
2020-05-01 10:26:20 +0100

    Setup hot-loader/react-dom

--- packages/web/webpack.dev.js
+++ packages/web/webpack.dev.js
@@ -4,6 +4,11 @@ const common = require("./webpack.common.js")
 
 module.exports = merge(common, {
   mode: "development",
+  resolve: {
+    alias: {
+      'react-dom': '@hot-loader/react-dom'
+    }
+  },
   devServer: {
     historyApiFallback: true,
     hot: true,
