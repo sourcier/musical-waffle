c4e265963c0276d639a2e80dbc489886c735bee0
Roger Rajaratnam <roger@sourcier.uk>
2020-05-03 20:28:15 +0100

    Add contact thank you page

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -10,6 +10,7 @@ import BlogPage from './components/routes/BlogPage'
 // import Home from './routes/Home'
 import NotFound from './components/routes/NotFound'
 import Contact from './components/routes/Contact'
+import ThankYou from './components/routes/Contact/ThankYou'
 import { treatments } from './libs/theme'
 
 export default hot((props) => (
@@ -20,6 +21,7 @@ export default hot((props) => (
         <Switch>
           {/* <Route path="/" exact component={Home} /> */}
           <Route path="/contact" exact component={Contact} />
+          <Route path="/contact/thank-you" exact component={ThankYou} />
           <Route path="/" exact component={Blog} />
           <Route path="/:slug" exact component={BlogPage} />
           <Route component={NotFound} />
