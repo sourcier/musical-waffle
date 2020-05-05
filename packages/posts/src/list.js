e500a35778b788e8db3c4ad0387f4233d1061076
Roger Rajaratnam <roger@sourcier.uk>
2020-05-05 17:41:50 +0100

    Update list endpoint to only return published and non internal pages

--- packages/posts/src/list.js
+++ packages/posts/src/list.js
@@ -1,15 +1,22 @@
-import * as dynamoDbLib from './libs/dynamodb'
-import { success, failure } from './libs/response'
+import * as dynamoDbLib from "./libs/dynamodb"
+import { success, failure } from "./libs/response"
 
 export default (event, context) => {
   return dynamoDbLib
-    .call('scan', {
+    .call("scan", {
       TableName: process.env.POSTS_TABLE_NAME,
-      FilterExpression: "published = :published AND attribute_not_exists(internal)",
+      FilterExpression:
+        "published = :published AND (attribute_not_exists(internal) OR internal = :internal)",
       ExpressionAttributeValues: {
         ":published": true,
+        ":internal": false,
       },
     })
-    .then((result) => success(result.Items))
+    .then((result) =>
+      success({
+        items: result.Items,
+        count: result.Count,
+      })
+    )
     .catch((e) => failure({ ...e }))
 }
