65a22dd4ac6a2fcf629882034b6ef5022d17875d
Roger Rajaratnam <roger@sourcier.uk>
2020-05-21 23:27:23 +0100

    WEB: Simplify dev/prod config merging

--- packages/web/src/libs/config.js
+++ packages/web/src/libs/config.js
@@ -1,3 +1,5 @@
+import merge from 'lodash/merge'
+
 const devConfig = {
   api: {
     posts: {
@@ -18,30 +20,24 @@ const devConfig = {
     userPoolWebClientId: '3nmtr2vpa6adlaum9l3g9b8j6n'
   },
   facebook: {
-    appId: process.env.FACEBOOK_APP_ID
+    appId: '318400859146823'
   }
 }
 
-const prodConfig = {
+const prodConfig = merge({}, devConfig, {
   api: {
     posts: {
-      name: 'posts',
-      endpoint: 'https://api.sourcier.uk',
-      region: 'eu-west-2'
+      endpoint: 'https://api.sourcier.uk'
     },
     mail: {
-      name: 'mail',
-      endpoint: 'https://api.sourcier.uk',
-      region: 'eu-west-2'
+      endpoint: 'https://api.sourcier.uk'
     }
   },
   cognito: {
-    region: 'eu-west-2',
-    identityPoolId: 'eu-west-2:5ec7e7c9-08b8-455e-906a-9714383f57d9'
-  },
-  facebook: {
-    appId: process.env.FACEBOOK_APP_ID
+    identityPoolId: 'eu-west-2:5ec7e7c9-08b8-455e-906a-9714383f57d9',
+    userPoolId: 'eu-west-2_UIfKjd60G',
+    userPoolWebClientId: '6e13a9u18s0spdog9er6jbhrk8'
   }
-}
+})
 
 export default process.env.APP_ENV === 'prod' ? prodConfig : devConfig
